fd52b65ad7fcbcaff11d0450b15e26af79ddd9e8
liuzhao@meicai.cn <tolz@qq.com>
2025-06-16 07:40:32 +0800

    fix bug

--- config/styles.ts
+++ config/styles.ts
@@ -11,12 +11,31 @@ export const styles = [
     "isMulti": false
   },
   {
-    "id": "styleTransfer",
-    "nameKey": "styles.styleTransfer.name",
-    "descriptionKey": "styles.styleTransfer.description",
-    "prompt": "Transform to $Style$, while maintaining women and pose unchanged",
-    "previewImage": "/styles/image2.png",
-    "resultImage": "/styles/p2.png",
+    "id": "objectRemoval",
+    "nameKey": "styles.objectRemoval.name",
+    "descriptionKey": "styles.objectRemoval.description",
+    "prompt": "Remove the watermarks and text from the picture, while keeping all other details unchanged",
+    "previewImage": "/styles/image9.png",
+    "resultImage": "/styles/p9.png",
+    "isMulti": false
+  },
+
+    {
+    "id": "ipAdapterStyleTransfer",
+    "nameKey": "styles.ipAdapterStyleTransfer.name",
+    "descriptionKey": "styles.ipAdapterStyleTransfer.description",
+    "prompt": "Using this image style, a woman and a man are looking up at the sky by the stream, 18-years-old",
+    "previewImage": "/styles/image10.png",
+    "resultImage": "/styles/p10.png",
+    "isMulti": false
+  },  
+  {
+    "id": "backgroundReplacement",
+    "nameKey": "styles.backgroundReplacement.name",
+    "descriptionKey": "styles.backgroundReplacement.description",
+    "prompt": "Change the background to in the classroom of the school, keep the subject in the exact same position and pose",
+    "previewImage": "/styles/image8.png",
+    "resultImage": "/styles/p8.png",
     "isMulti": false
   },
   {
@@ -37,15 +56,7 @@ export const styles = [
     "resultImage": "/styles/p4.png",
     "isMulti": true
   },
-  {
-    "id": "modelProductFusion",
-    "nameKey": "styles.modelProductFusion.name",
-    "descriptionKey": "styles.modelProductFusion.description",
-    "prompt": "The change involved a woman holding a blue bag for a live stream display, maintaining the consistency of the character unchanged",
-    "previewImage": "/styles/image5.png",
-    "resultImage": "/styles/p5.png",
-    "isMulti": false
-  },
+
   // {
   //   "id": "modelProductFusion",
   //   "nameKey": "styles.modelProductFusion.name",
@@ -66,33 +77,9 @@ export const styles = [
   },
 
 
-  {
-    "id": "backgroundReplacement",
-    "nameKey": "styles.backgroundReplacement.name",
-    "descriptionKey": "styles.backgroundReplacement.description",
-    "prompt": "Change the background to in the classroom of the school, keep the subject in the exact same position and pose",
-    "previewImage": "/styles/image8.png",
-    "resultImage": "/styles/p8.png",
-    "isMulti": false
-  },
-  {
-    "id": "objectRemoval",
-    "nameKey": "styles.objectRemoval.name",
-    "descriptionKey": "styles.objectRemoval.description",
-    "prompt": "Remove the watermarks and text from the picture, while keeping all other details unchanged",
-    "previewImage": "/styles/image9.png",
-    "resultImage": "/styles/p9.png",
-    "isMulti": false
-  },
-  {
-    "id": "ipAdapterStyleTransfer",
-    "nameKey": "styles.ipAdapterStyleTransfer.name",
-    "descriptionKey": "styles.ipAdapterStyleTransfer.description",
-    "prompt": "Using this image style, a woman and a man are looking up at the sky by the stream, 18-years-old",
-    "previewImage": "/styles/image10.png",
-    "resultImage": "/styles/p10.png",
-    "isMulti": false
-  },  
+
+
+
   {
     "id": "addModelToClothing",
     "nameKey": "styles.addModelToClothing.name",
@@ -100,6 +87,24 @@ export const styles = [
     "prompt": "Add a model to the clothing, keep the clothing and the background unchanged",
     "previewImage": "/styles/image11.png",
     "resultImage": "/styles/p11.png",
+    "isMulti": false
+  },
+  {
+    "id": "modelProductFusion",
+    "nameKey": "styles.modelProductFusion.name",
+    "descriptionKey": "styles.modelProductFusion.description",
+    "prompt": "The change involved a woman holding a blue bag for a live stream display, maintaining the consistency of the character unchanged",
+    "previewImage": "/styles/image5.png",
+    "resultImage": "/styles/p5.png",
+    "isMulti": false
+  },
+  {
+    "id": "styleTransfer",
+    "nameKey": "styles.styleTransfer.name",
+    "descriptionKey": "styles.styleTransfer.description",
+    "prompt": "Transform to $Style$, while maintaining women and pose unchanged",
+    "previewImage": "/styles/image2.png",
+    "resultImage": "/styles/p2.png",
     "isMulti": false
   },
   {
